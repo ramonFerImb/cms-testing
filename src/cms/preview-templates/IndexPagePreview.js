33dee3dfc8251c5ef642d4db81a37e51691e45d9
ramonFerImb <ramon.fernandez.imbernon@everis.com>
2021-03-10 18:05:33 +0100

    changed retrived information now from data

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -5,13 +5,7 @@ import { IndexPageTemplate } from "../../templates/index-page";
 const IndexPagePreview = ({ entry, getAsset }) => {
   const data = entry.getIn(["data"]).toJS();
   console.log(data);
-  return (
-    <IndexPageTemplate
-      lang={entry.getIn(["data", "lang"])}
-      carrousel={entry.getIn(["data", "carrousel"]) || []}
-      about={entry.getIn(["data", "about"])}
-    />
-  );
+  return <IndexPageTemplate lang={data.lang} carrousel={data.carrousel} about={data.about} />;
 };
 
 IndexPagePreview.propTypes = {
