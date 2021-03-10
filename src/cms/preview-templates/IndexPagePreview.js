5e86c6dd97fa13e2d41db492ce44d8a869c00d42
ramonFerImb <ramon.fernandez.imbernon@everis.com>
2021-03-10 16:35:08 +0100

    testing entry

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -2,10 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 import { IndexPageTemplate } from "../../templates/index-page";
 
-const IndexPagePreview = ({ entry, getAsset }) => {
+const IndexPagePreview = (props, { entry, getAsset }) => {
   const carrousel = entry.getIn(["data", "carrousel"]).toJS();
   const about = entry.getIn(["data", "about"]).toJS();
   const lang = entry.getIn(["data", "lang"]).toJS();
+  console.log(props);
 
   return <div>This is a test</div>;
   // return <IndexPageTemplate lang={lang} carrousel={carrousel || []} carrousel={about} />;
