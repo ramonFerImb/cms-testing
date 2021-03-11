1e6a21aef12e9cf635e52597e3b904fad0d3c866
ramonFerImb <ramon.fernandez.imbernon@everis.com>
2021-03-11 17:05:26 +0100

    change to new

---
+++ src/cms/preview-templates/NewsPagePreview.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { UpdatesPageTemplate } from "../../templates/updates-page";
+
+const NewsPagePreview = ({ entry, getAsset }) => {
+  const data = entry.getIn(["data"]).toJS();
+  return (
+    <UpdatesPageTemplate
+      lang={data.lang}
+      carrousel={data.carrousel}
+      about={data.about}
+      productsSection={data.productsSection}
+      certificationsSection={data.certificationsSection}
+      updatesSection={data.updatesSection}
+    />
+  );
+};
+
+export default NewsPagePreview;
