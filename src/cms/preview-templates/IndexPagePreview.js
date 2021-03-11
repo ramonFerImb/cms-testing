b5b481a6e8ba26982392620d559c6c323358a48a
ramonFerImb <ramon.fernandez.imbernon@everis.com>
2021-03-11 12:08:52 +0100

    added props in template

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -5,7 +5,16 @@ import { IndexPageTemplate } from "../../templates/index-page";
 const IndexPagePreview = ({ entry, getAsset }) => {
   const data = entry.getIn(["data"]).toJS();
   console.log(data);
-  return <IndexPageTemplate lang={data.lang} carrousel={data.carrousel} about={data.about} />;
+  return (
+    <IndexPageTemplate
+      lang={data.lang}
+      carrousel={data.carrousel}
+      about={data.about}
+      productsSection={data.productsSection}
+      certificationsSection={data.certificationsSection}
+      updatesSection={data.updatesSection}
+    />
+  );
 };
 
 IndexPagePreview.propTypes = {
