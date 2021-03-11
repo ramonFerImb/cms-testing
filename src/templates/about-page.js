bcdad21e98b9633316b998a3704074843ca38d01
ramonFerImb <ramon.fernandez.imbernon@everis.com>
2021-03-11 16:00:03 +0100

    changed updates md and component

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import { useTranslate } from "../utils/useTranslate";
 import { Layout, Section, Carrousel, Button, Card } from "./../components";
